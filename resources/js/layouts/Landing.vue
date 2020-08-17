8ceb4589e8096eda3b792e4ca4c4b8b94ba97301
Abdurozzaq Nurul Hadi <Abdurozzaq@outlook.com>
2020-08-17 22:24:24 +0700

    Fixed Landing Page Action Buttons

--- resources/js/layouts/Landing.vue
+++ resources/js/layouts/Landing.vue
@@ -13,11 +13,15 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn href="/login" text>
+      <v-btn v-if="isLoggedIn == false" href="/login" text>
         LOGIN
       </v-btn>
-      <v-btn href="/register" text>
+      <v-btn v-if="isLoggedIn == false" href="/register" text>
         REGISTER
+      </v-btn>
+
+      <v-btn v-if="isLoggedIn == true" @click="goToDash" text>
+        GO TO DASH
       </v-btn>
     </v-app-bar>
 
@@ -29,19 +33,83 @@
 
         <!-- If using vue-router -->
         <router-view></router-view>
+
+        <v-snackbar top v-model="snack" :timeout="3000" :color="snackColor">
+          {{ snackText }}
+
+          <template v-slot:action="{ attrs }">
+            <v-btn v-bind="attrs" text @click="snack = false">Close</v-btn>
+          </template>
+        </v-snackbar>
+
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
+  import axios from 'axios'
   export default {
     name: 'LandingPage',
     data() {
       return {
+        isLoggedIn: false,
 
+        snack: false,
+        snackColor: null,
+        snackText: null,
       }
-    }
+    }, // End of Data
+
+    methods: {
+      checkAuth: function() {
+        let currentObj = this
+        let token = localStorage.getItem('userToken')
+
+        if (token) {
+          currentObj.isLoggedIn = true
+        } else {
+          currentObj.isLoggedIn = false
+        }
+      },
+
+      goToDash: function() {
+        let currentObj = this
+
+        currentObj.snack = true
+        currentObj.snackColor = 'success'
+        currentObj.snackText = 'Redirecting To Dashboard... Please Wait A Minute!'
+
+
+        let token = localStorage.getItem('userToken')
+
+        axios.get('api/auth/me', {
+        headers: {
+          Authorization: 'Bearer ' + token,
+          withCredentials: true //the token is a variable which holds the token
+        }
+        })
+        .then(function (response) {
+            // handle success
+            let userRole = response.data.role
+            if (userRole == "admin") {
+              currentObj.$router.push('/siAdmino')
+            } else {
+              currentObj.$router.push('/home')
+            }
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+      }
+    }, // End of Methods
+
+    mounted: function() {
+      let currentObj = this
+
+      currentObj.checkAuth()
+    }, // End of Mounted
   }
 </script>
 
