903915eb075f1f579f1002f3605fdc37b887d307
Abdurozzaq Nurul Hadi <Abdurozzaq@outlook.com>
2021-01-07 23:19:13 +0700

    Updated AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // when APP_ENV is not local then set it to https true
+        If (env('APP_ENV') !== 'local') {
+          $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
